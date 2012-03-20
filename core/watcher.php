b763ba4eb483d9b63ed9029098239bf4cb72e42f
Koen Schmeets <k.schmeets@gmail.com>
2012-03-20 17:26:02 +0100

    Updating Watcher example and indicating it's a TODO

--- core/watcher.php
+++ core/watcher.php
@@ -1,8 +1,9 @@
 <?php namespace LCQRS;
 
 class Watcher {
+	// TODO
 }
 
 Watcher::for('event: AccountRegistered', array('as' => 'registered'))->and('command: RemoveAccount', array('as' => 'removed'))->max('15 minutes')->then(function($messages) {
-	CommandMessage::send(new AskReasonForFastRemovalOfAccountViaEmail($messages['registered'], $messages['removed']));
+	Bus::send(new AskReasonForFastRemovalOfAccountViaEmail($messages['registered'], $messages['removed']));
 });
